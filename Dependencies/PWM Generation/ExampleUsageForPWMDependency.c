242d64f848135567f32ff1e8e558e63cda2d7151
ZacharyDownum <zdownum@gmail.com>
2018-02-11 16:03:10 -0600

    Added Documentation for Previously Completed Subprojects
    Added a program to the PWM Generation Dependency to showcase how the dependency can be used to generate PWM signals.
    
    Added a Readme to the PWM Generation Dependency to explain how it works, which pins it uses, and its limitations.
    
    Added a list of Critical Pins for the Thrust Testing program--the specific pins that are used in the program and what they are used for.

---
+++ Dependencies/PWM Generation/ExampleUsageForPWMDependency.c
@@ -0,0 +1,80 @@
+/*
+ * File:   output_signal_generation_driver.c
+ * Author: Zachary Downum
+ */
+
+#include "mcc_generated_files/mcc.h"
+
+//FCY is based off _XTAL_FREQ, the current system clock
+//(see system_configuration.h)
+#define FCY (_XTAL_FREQ / 2)
+
+#include <stdlib.h>
+#include <libpic30.h>
+#include <xc.h>
+
+#include "PWM.h"
+
+//basic initialization for all pins
+void PIC_Initialization(void)
+{
+    //changes all pins to digital
+    ANSB = 0x0000;
+    Nop();
+    
+    //changes all pins to output (except for RP4, which is an input only pin)
+    TRISB = 0x0000;
+    Nop();
+}
+
+void PWM_Module_Initialize(PWM_Module* left_motor, PWM_Module* right_motor)
+{
+    left_motor->Initialize = PWM_LeftMotorInitialize;
+    left_motor->GetDutyCycle = PWM_GetLeftMotorDutyCycle;
+    left_motor->GetFrequency = PWM_GetLeftMotorFrequency;
+    left_motor->UpdateDutyCycle = PWM_UpdateLeftMotorDutyCycle;
+    left_motor->UpdateFrequency = PWM_UpdateLeftMotorFrequency;
+    
+    right_motor->Initialize = PWM_RightMotorInitialize;
+    right_motor->GetDutyCycle = PWM_GetRightMotorDutyCycle;
+    right_motor->GetFrequency = PWM_GetRightMotorFrequency;
+    right_motor->UpdateDutyCycle = PWM_UpdateRightMotorDutyCycle;
+    right_motor->UpdateFrequency = PWM_UpdateRightMotorFrequency;
+}
+
+
+int main(void)
+{
+    SYSTEM_Initialize();
+    PIC_Initialization();
+
+    PWM_Module Left_Motor;
+    PWM_Module Right_Motor;
+    PWM_Module_Initialize(&Left_Motor, &Right_Motor);
+    
+    Left_Motor.Initialize(&Left_Motor);
+    Right_Motor.Initialize(&Right_Motor);
+    
+    
+    Left_Motor.frequency = 20000;
+    Left_Motor.UpdateFrequency(&Left_Motor);
+    
+    Right_Motor.frequency = 20000;
+    Right_Motor.UpdateFrequency(&Right_Motor);
+    
+    while(true)
+    {
+        int i;
+        for (i = 0; i <= 100; i += 10)
+        {
+            Left_Motor.duty_cycle_percentage = i;
+            Left_Motor.UpdateDutyCycle(&Left_Motor);
+            
+            Right_Motor.duty_cycle_percentage = i;
+            Right_Motor.UpdateDutyCycle(&Right_Motor);
+            __delay_ms(5000);
+        }
+    }
+    
+    return -1;
+}
