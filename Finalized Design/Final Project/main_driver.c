56f202c4fb2e947364f28d0a055bccc3d2c344ab
ZacharyDownum <zdownum@gmail.com>
2018-04-22 20:24:29 -0500

    Fixed Almost Everything With Control System
    Fixed direction of relays.
    
    Fixed PWMs staying high even after they are supposed to shut off.
    
    IMPORTANT:
    The one issue that is not fixed is that after the wireless controller is disconnected. the relays remain on and continue to power the engines.  THIS NEEDS TO BE FIXED.

--- Finalized Design/Final Project/main_driver.c
+++ Finalized Design/Final Project/main_driver.c
@@ -17,19 +17,24 @@
 #include "InputCapture.h"
 
 //these were experimentally derived, so these may not be the optimal values
-#define MINIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)7.29
-#define MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)13.79
-#define MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE (double)((MINIMUM_INPUT_SIGNAL_DUTY_CYCLE + MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE) / 2)
+#define SWITCH_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)5.563
+#define SWITCH_MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)12.453
+#define SWITCH_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE (double)((SWITCH_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE + SWITCH_MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE) / 2)
 
-#define STEERING_MIN_INPUT_SIGNAL_DUTY_CYCLE (double)7.39
-#define STEERING_MAX_INPUT_SIGNAL_DUTY_CYCLE (double)13.89
-#define STEERING_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE (double)((MINIMUM_INPUT_SIGNAL_DUTY_CYCLE + MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE) / 2)
+//these were experimentally derived, so these may not be the optimal values
+#define THROTTLE_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)5.563
+#define THROTTLE_MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE (double)12.453
+#define THROTTLE_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE (double)((THROTTLE_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE + THROTTLE_MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE) / 2)
+
+#define STEERING_MIN_INPUT_SIGNAL_DUTY_CYCLE (double)7.462
+#define STEERING_MAX_INPUT_SIGNAL_DUTY_CYCLE (double)13.813
+#define STEERING_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE (double)((STEERING_MIN_INPUT_SIGNAL_DUTY_CYCLE + STEERING_MAX_INPUT_SIGNAL_DUTY_CYCLE) / 2)
 
 //Setting the INCREMENT_ADJUSTMENT_FACTOR to 100 achieves an output duty cycle that goes from 0% to 100%
 //make the INCREMENT_ADJUSTMENT_FACTOR smaller to make the maximum output duty cycle % smaller
 //make the INCREMENT_ADJUSTMENT_FACTOR larger to make the maximum output duty cycle % larger (not recommended as 100% should be the absolute max)
 #define THROTTLE_INCREMENT_ADJUSTMENT_FACTOR 10
-#define PROPULSION_THROTTLE_CYCLE_INCREMENT ((double)(MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE - MINIMUM_INPUT_SIGNAL_DUTY_CYCLE) / THROTTLE_INCREMENT_ADJUSTMENT_FACTOR)
+#define PROPULSION_THROTTLE_CYCLE_INCREMENT ((double)(THROTTLE_MAXIMUM_INPUT_SIGNAL_DUTY_CYCLE - THROTTLE_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE) / THROTTLE_INCREMENT_ADJUSTMENT_FACTOR)
 #define PROPULSION_THROTTLE_SERVO_OFFSET 1.8
 
 #define STEERING_INCREMENT_ADJUSTMENT_FACTOR 100
@@ -102,10 +107,10 @@ void Kill_Switch_Initialize(void)
 
 int main(void)
 {
-	double averagedPropulsionThrottleDutyCycle = MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
+	double averagedPropulsionThrottleDutyCycle = SWITCH_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
 	double averagedPropulsionSteeringDutyCycle = STEERING_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE;
-    double averagedKillSwitchDutyCycle = MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
-    double averagedBrakeSwitchDutyCycle = MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
+    double averagedKillSwitchDutyCycle = SWITCH_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
+    double averagedBrakeSwitchDutyCycle = SWITCH_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE;
     double previousPositionOfPropulsionMotor = 0;
 	
     SYSTEM_Initialize();
@@ -137,7 +142,7 @@ int main(void)
     
     turn_propulsion_engine_output.frequency = 400;
     turn_propulsion_engine_output.UpdateFrequency(&turn_propulsion_engine_output);
-    turn_propulsion_engine_output.dutyCyclePercentage = 100;
+    turn_propulsion_engine_output.dutyCyclePercentage = 0;
     turn_propulsion_engine_output.UpdateDutyCycle(&turn_propulsion_engine_output);
     __delay_ms(1000);
     
@@ -149,16 +154,11 @@ int main(void)
         averagedPropulsionSteeringDutyCycle = (averagedPropulsionSteeringDutyCycle + propulsion_direction_motor_input.dutyCyclePercentage) / 2;
 		propulsion_throttle_servo_input.Update(&propulsion_throttle_servo_input);
 		averagedPropulsionThrottleDutyCycle = (averagedPropulsionThrottleDutyCycle + propulsion_throttle_servo_input.dutyCyclePercentage) / 2;
-		
-        if (averagedPropulsionSteeringDutyCycle > STEERING_MAX_INPUT_SIGNAL_DUTY_CYCLE)
-        {
-            averagedPropulsionSteeringDutyCycle = STEERING_MAX_INPUT_SIGNAL_DUTY_CYCLE;
-        }
         
         //this is to regulate the duty cycle that is sent to the servo so that it falls within the acceptable range for
         //the servo that is being used by the project.
         //this duty cycle should be approximately between 5% and 15% (with 10% being directly in the center, or 90 degrees of motion in a 180 degree servo)
-        propulsion_throttle_servo_output.dutyCyclePercentage = (averagedPropulsionThrottleDutyCycle - MINIMUM_INPUT_SIGNAL_DUTY_CYCLE) / PROPULSION_THROTTLE_CYCLE_INCREMENT + PROPULSION_THROTTLE_SERVO_OFFSET;
+        propulsion_throttle_servo_output.dutyCyclePercentage = (averagedPropulsionThrottleDutyCycle - THROTTLE_MINIMUM_INPUT_SIGNAL_DUTY_CYCLE) / PROPULSION_THROTTLE_CYCLE_INCREMENT + PROPULSION_THROTTLE_SERVO_OFFSET;
         
         //This is here to account for minor variations that put the input duty cycle above or below
         //the minimum or maximum input signal duty (which could cause undefined behavior on the output signal)
@@ -174,29 +174,27 @@ int main(void)
         }
         
         propulsion_throttle_servo_output.UpdateDutyCycle(&propulsion_throttle_servo_output);
-		
         
         //This is here to account for minor variations that put the input duty cycle above or below
         //the minimum or maximum input signal duty (which could cause undefined behavior on the output signal)
         //this is a binary interpretation of an input signal that could have multiple values, treating it like the switch it represents
-        if (averagedKillSwitchDutyCycle < MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
-        {
-            LATAbits.LATA0 = 0;
-            LATAbits.LATA1 = 0;
-        }
-        else if (averagedKillSwitchDutyCycle >= MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
+        if (averagedKillSwitchDutyCycle < SWITCH_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE || (LATBbits.LATB4 == 1 && LATBbits.LATB5 == 1 && LATBbits.LATB6 == 1 && LATBbits.LATB8 == 1))
         {
             LATAbits.LATA0 = 1;
             LATAbits.LATA1 = 1;
         }
-		
+        else if (averagedKillSwitchDutyCycle >= SWITCH_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
+        {
+            LATAbits.LATA0 = 0;
+            LATAbits.LATA1 = 0;
+        }
 		
 		propulsion_brake_input.Update(&propulsion_brake_input);
         averagedBrakeSwitchDutyCycle = (100 * averagedBrakeSwitchDutyCycle + propulsion_brake_input.dutyCyclePercentage) / 101;
 		
         
 		int discreteLocation = 0;
-        if (averagedBrakeSwitchDutyCycle < MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
+        if (averagedBrakeSwitchDutyCycle < SWITCH_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
         {
 			//represents a leftward turn of the propulsion engine
 			//at the moment, the frequency of the PWM signal * the number of counts per trigger = 800 (counts per rotation) * rotations per second
@@ -204,6 +202,16 @@ int main(void)
 			//if PWM frequency * counts per trigger > 800 * RPS, then the user input will experience a delay in controlling the stepper motor
 			//if PWM frequency * counts per trigger < 800 * RPS, then the user input will cause jerking in the stepper motor response
 			double preciseLocation = (averagedPropulsionSteeringDutyCycle - STEERING_MIN_INPUT_SIGNAL_DUTY_CYCLE) / PROPULSION_STEERING_CYCLE_INCREMENT;
+            
+            if (preciseLocation < 0)
+            {
+                preciseLocation = 0;
+            }
+            else if (preciseLocation > 100)
+            {
+                preciseLocation = 100;
+            }
+            
             if (preciseLocation < previousPositionOfPropulsionMotor + 2 && preciseLocation > previousPositionOfPropulsionMotor - 2)
             {
                 preciseLocation = previousPositionOfPropulsionMotor;
@@ -230,10 +238,10 @@ int main(void)
 			}
 			else if (discreteLocation < -COUNTS_FOR_90_DEGREE_TURN - 1)
 			{
-				discreteLocation = -COUNTS_FOR_90_DEGREE_TURN + 1;
+				discreteLocation = -COUNTS_FOR_90_DEGREE_TURN - 1;
 			}
         }
-        else
+        else if (averagedBrakeSwitchDutyCycle >= SWITCH_MIDPOINT_INPUT_SIGNAL_DUTY_CYCLE)
         {
 			if (stepper_motor_counter_input.numberOfCounts >= 0)
 			{
@@ -272,7 +280,7 @@ int main(void)
             //the motor holds its position
             else
             {
-                turn_propulsion_engine_output.dutyCyclePercentage = 100;
+                turn_propulsion_engine_output.dutyCyclePercentage = 0;
                 turn_propulsion_engine_output.UpdateDutyCycle(&turn_propulsion_engine_output);
             }
         }
